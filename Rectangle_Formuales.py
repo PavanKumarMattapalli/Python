786bd631da3015f2b0248a6b9217505806e98960
Pavan Kumar Mattapalli <79364585+PavanKumarMattapalli@users.noreply.github.com>
2021-06-03 09:32:07 +0530

    Add files via upload

---
+++ Rectangle_Formuales.py
@@ -0,0 +1,19 @@
+from math import *
+
+
+l,b=map(float,input().split())
+area=l*b
+perimeter=2*(l+b)
+diagnoal=sqrt(l**2+b**2)
+print("%.2f"%area,end=" ")
+print("%.2f"%perimeter,end=" ")
+print("%.2f"%diagnoal,end=" ")
+
+
+
+"""
+Rectangle_Formuales
+area:l*b
+perimeter :2*(l+b)
+diagonal:(l**2+b**2)
+"""
