7c64caecfe7af0fc6b924d29039d686f18a9bc36
Pavan Mattapalli <79364585+PavanKumarMattapalli@users.noreply.github.com>
2021-06-19 21:55:04 +0530

    Add files via upload

---
+++ Finding Greatest Among Three Numbers.py
@@ -0,0 +1,11 @@
+num1=int(input("Enter First Number :"))
+num2=int(input("Enter Second Number :"))
+num3=int(input("Enter Third Number :"))
+if(num1>num2 and num1>num3):
+    print(num1,"is bigger.")
+elif(num2>num1 and num2>num3):
+    print(num2,"is bigger")
+elif(num3>num2 and num3>num1):
+    print(num3,"is bigger")
+else:
+    print("Wrong Input")
