833481148cfe8aa6a49109cc7f8c5519aaaa7ae9
Pavan Mattapalli <79364585+PavanKumarMattapalli@users.noreply.github.com>
2021-06-24 16:50:43 +0530

    Add files via upload

---
+++ Closed_Paths.py
@@ -0,0 +1,19 @@
+def cp(n):
+    if n==1 or n==2 or n==3 or n==5 and n==7:
+        return 0
+    elif n==8:
+        return 2
+    else:
+        return 1
+
+def cpsum(t):
+    s=0
+    while t:
+        rem=t%10
+        t=t//10
+        s+=cp(rem)
+    return s
+t=int(input())
+print(cpsum(t))
+
+    
